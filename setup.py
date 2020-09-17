c7585414c271597579a6f9e51224275bc204cdc3
remi calizzano <remi.calizzano@gmail.com>
2020-09-17 14:17:24 +0200

    Change little things

--- setup.py
+++ setup.py
@@ -15,4 +15,13 @@ setup(
     install_requires=["transformers>=3.1.0", "datasets", "onnx", "onnxruntime"],
     entry_points={"console_scripts": ["benchmark-for-transformers-run=benchmark_for_transformers.run_cli:main"]},
     python_requires=">=3.6.0",
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Scientific/Engineering :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
